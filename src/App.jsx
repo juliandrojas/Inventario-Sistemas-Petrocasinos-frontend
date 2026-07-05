118525324c9243a3b27bad5083161b2c04fe3016
juliandrojas <ingjulianrojas14@gmail.com>
2026-07-04 23:50:51 -0500

    fix("Infiinity loop in App.jsx")

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./views/Login";
 import Register from "./views/Register";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<Navigate to="/login" replace />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        {/* Route Comodin */}
+        {/* Route Comodín */}
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </>
