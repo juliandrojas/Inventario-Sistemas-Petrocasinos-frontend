33b428589bd31097ff9b5493f5a555c416bb5602
juliandrojas <ingjulianrojas14@gmail.com>
2026-07-04 23:46:00 -0500

    fix("Provisional Navigation")

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./views/Login";
 import Register from "./views/Register";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Navigate to="/login" replace />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </>
